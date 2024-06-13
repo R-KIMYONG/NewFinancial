3df2ec31e0189e531d713e0bcd9db5407f28bc3c
kimyong <right4570@naver.com>
2024-06-13 23:21:24 +0900

    fix: 오타 수정

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { GlobalStyle } from "./GlobalStyle";
 
-``
+
 function App() {
   return (
     <>
